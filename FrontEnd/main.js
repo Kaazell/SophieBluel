8f27ce761c1452c422dcd3c085e10b6cfdf0db40
Atef Lachhab <atef.lachhab1@gmail.com>
2023-06-16 10:41:41 -0400

    css modal done

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -93,79 +93,90 @@ connected()
 
 // Modal
 
-let modal = null
-
 const openModal = function (e) {
-    e.preventDefault()
-
-    const target = document.querySelector(e.target.getAttribute('href'))
+    e.preventDefault();
+  
+    const target = document.querySelector(e.target.getAttribute('href'));
     target.style.display = null;
     target.removeAttribute('aria-hidden');
     target.setAttribute('aria-modal', 'true');
-    modal = target
+    modal = target;
     modal.addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
-    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
-}
-
-const closeModal = function (e) {
-    if (modal === null) return
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute('href'))
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+  };
+  
+  const closeModal = function (e) {
+    if (modal === null) return;
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
     modal.style.display = 'none';
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
     modal.removeEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').removeEventListener( 'click', closeModal)
-    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
-    modal = null
-}
-
-const stopPropagation = function(e) {
-    e.stopPropagation()
-}
-
-document.querySelectorAll(".js-modal").forEach(a => {
-    a.addEventListener('click', openModal)
-})
-
-window.addEventListener('keydown', function(e) {
-    if(e.key === "Escape" || e.key ==="Esc"){
-    closeModal(e)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+  };
+  
+  const stopPropagation = function (e) {
+    e.stopPropagation();
+  };
+  
+  document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener('click', openModal);
+  });
+  
+  window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+      closeModal(e);
     }
-})
-
-function displayGalleryModal(d){
-    const modalDisp = document.querySelector('.modal-wrapper')
+  });
+  
+  const modalDisp = document.querySelector('.modal-wrapper');
+  
+  function displayGalleryModal(d) {
     const gallery = document.createElement('div');
-    modalDisp.append(gallery)
-    gallery.className = "works-modal"    
-    for(let i = 0; i < d.length; i++){
-        let figure = document.createElement('figure');
-        let img = document.createElement('img');
-        let crossImg = document.createElement('i');
-        let figCaption = document.createElement('figcaption');
-
-        crossImg.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
-        img.src = d[i].imageUrl;
-        figCaption.innerText = d[i].title;
-
-        gallery.append(figure);
-        figure.append(crossImg)
-        figure.append(img);
-        figure.append("éditer");
+    modalDisp.append(gallery);
+    gallery.className = "works-modal";
+    for (let i = 0; i < d.length; i++) {
+      let figure = document.createElement('figure');
+      let img = document.createElement('img');
+      let crossImg = document.createElement('i');
+      let figCaption = document.createElement('figcaption');
+  
+      crossImg.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+      img.src = d[i].imageUrl;
+      figCaption.innerText = d[i].title;
+  
+      gallery.append(figure);
+      figure.append(crossImg);
+      figure.append(img);
+      figure.append("éditer");
     }
-}
-
-async function getJsonModal(){
-    const requestWorks = await fetch(url + "/works",{
+  }
+  
+  async function getJsonModal() {
+    try {
+      const response = await fetch(`${url}/works`, {
         method: 'GET'
-    })
-    if(!requestWorks.ok){
-        alert('Une erreur est survenue.')
-    } else {
-        dataModal = await requestWorks.json();
-        displayGalleryModal(dataModal);
+      });
+      if (!response.ok) {
+        throw new Error('Une erreur est survenue.');
+      }
+      const dataModal = await response.json();
+      displayGalleryModal(dataModal);
+      const hr = document.createElement('hr');
+      const galleryEdit = document.createElement('div');
+      galleryEdit.setAttribute('id','gallery-edit-buttons')
+      galleryEdit.innerHTML = "<button class='gallery-edit'>Ajouter une photo</button><button id='delete-gallery' class='delete-gallery'>Supprimer la galerie</button>";
+      modalDisp.append(hr);
+      modalDisp.append(galleryEdit);
+    } catch (error) {
+      alert(error.message);
     }
-}
-getJsonModal()
+  }
+  
+  getJsonModal();
+
+  
